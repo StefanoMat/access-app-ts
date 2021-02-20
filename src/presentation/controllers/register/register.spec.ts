c8f72e21dd9e06e4ea53d5bca88c16ce9d2ce7e5
stefano oliveira <stefano.oliveira@paipe.co>
2021-02-20 16:50:32 -0300

    test: refactor with DI

--- src/presentation/controllers/register/register.spec.ts
+++ src/presentation/controllers/register/register.spec.ts
@@ -1,51 +1,91 @@
 import { RegisterController } from './register'
-import { badRequest } from '../helpers/http-helper'
-import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+import { AddUser, AddUserModel, UserModel, HttpRequest } from '../../controllers/register/register-protocols'
 
+const makeAddUser = (): AddUser => {
+  class AddUserStub implements AddUser {
+    async add (account: AddUserModel): Promise<UserModel> {
+      return await new Promise(resolve => resolve(makeFakeUser()))
+    }
+  }
+
+  return new AddUserStub()
+}
+
+const makeFakeUser = (): UserModel => ({
+  id: 1,
+  name: 'any_valid_name',
+  email: 'any_valid_email@mail.com',
+  password: 'any_valid_pass',
+  typeId: 1,
+  status: true
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_pass',
+    typeId: 'valid_type_id',
+    status: 'valid_status'
+  }
+})
+interface SutTypes {
+  sut: RegisterController
+  addUserStub: AddUser
+}
+
+const makeSut = (): SutTypes => {
+  const addUserStub = makeAddUser()
+  const sut = new RegisterController(addUserStub)
+  return { sut, addUserStub }
+}
 describe('Register Controller', () => {
   test('Should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
         password: 'any_pass',
-        tipoId: 1,
+        typeId: 1,
         status: true
       }
     }
-    const controller = new RegisterController()
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 
   test('Should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         password: 'any_pass',
-        tipoId: 1,
+        typeId: 1,
         status: true
       }
     }
-    const controller = new RegisterController()
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
-        tipoId: 1,
+        typeId: 1,
         status: true
       }
     }
-    const controller = new RegisterController()
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 
-  test('Should return 400 if no tipoId is provided', async () => {
+  test('Should return 400 if no typeId is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -54,22 +94,21 @@ describe('Register Controller', () => {
         status: true
       }
     }
-    const controller = new RegisterController()
-    const httpResponse = await controller.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('tipoId')))
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('typeId')))
   })
 
   test('Should return 400 if no status is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_pass',
-        tipoId: 1
+        typeId: 1
       }
     }
-    const controller = new RegisterController()
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('status')))
   })
 })
