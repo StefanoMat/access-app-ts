e3089da7ebadea82fe2f14287c77b559626ba7f3
stefano oliveira <stefano.oliveira@paipe.co>
2021-02-20 16:00:22 -0300

    test: name provided

---
+++ src/presentation/controllers/register/register.spec.ts
@@ -0,0 +1,20 @@
+import { RegisterController } from './register'
+import { badRequest } from '../helpers/http-helper'
+import { MissingParamError } from '../errors/missing-param-error'
+
+describe('Register Controller', () => {
+  test('Should return 400 if no name is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_pass',
+        tipo_id: 1,
+        status: true
+      }
+    }
+    const controller = new RegisterController()
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+})
