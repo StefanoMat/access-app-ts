d5a120d00d2c80bb6e702bafdf588b2fa9c5130e
stefano oliveira <stefano.oliveira@paipe.co>
2021-02-20 16:24:11 -0300

    test: param validations

--- src/presentation/controllers/register/register.spec.ts
+++ src/presentation/controllers/register/register.spec.ts
@@ -8,12 +8,68 @@ describe('Register Controller', () => {
       body: {
         email: 'any_email@mail.com',
         password: 'any_pass',
-        tipo_id: 1,
+        tipoId: 1,
         status: true
       }
     }
     const controller = new RegisterController()
     const httpResponse = await controller.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+
+  test('Should return 400 if no email is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_pass',
+        tipoId: 1,
+        status: true
+      }
+    }
+    const controller = new RegisterController()
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        tipoId: 1,
+        status: true
+      }
+    }
+    const controller = new RegisterController()
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should return 400 if no tipoId is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_pass',
+        status: true
+      }
+    }
+    const controller = new RegisterController()
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('tipoId')))
+  })
+
+  test('Should return 400 if no status is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_pass',
+        tipoId: 1
+      }
+    }
+    const controller = new RegisterController()
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('status')))
   })
 })
--- src/presentation/controllers/register/register.ts
+++ src/presentation/controllers/register/register.ts
@@ -5,12 +5,14 @@ import { MissingParamError } from '../errors'
 export class RegisterController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name']
+      const requiredFields = ['name', 'email', 'password', 'tipoId', 'status']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
+
+      return ok('')
     } catch (error) {
       return serverError(error)
     }
