1e438c2037ba403cf96672c0692326a4d2426b71
stefano oliveira <stefano.oliveira@paipe.co>
2021-02-21 17:49:51 -0300

    test: return 500 and 200 cases

--- src/presentation/controllers/list/list.spec.ts
+++ src/presentation/controllers/list/list.spec.ts
@@ -2,6 +2,8 @@ import { ListController } from './list'
 import { GetUser, GetUserParams } from '../../../domain/usecases/get-user'
 import { UserModel } from '../../../domain/models/user'
 import { HttpRequest } from '../../protocols'
+import { serverError, ok } from '../../helpers/http-helper'
+import { ServerError } from '../../errors'
 
 const makeGetUser = (): GetUser => {
   class GetUserStub implements GetUser {
@@ -44,5 +46,21 @@ describe('List Controller', () => {
     const getByIdSpy = jest.spyOn(getUserStub, 'getById')
     await sut.handle(makeFakeRequest())
     expect(getByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return 500 if GetUser throws', async () => {
+    const { sut, getUserStub } = makeSut()
+    jest.spyOn(getUserStub, 'getById').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should return 200 if valid param is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeUser()))
   })
 })
