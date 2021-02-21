7bcf713c7d9bd1082d1a6fb0c758d939625a03c4
stefano oliveira <stefano.oliveira@paipe.co>
2021-02-21 19:24:17 -0300

    test: add BcryptAdapter

---
+++ src/infra/criptography/bcrypt-adaptet.spec.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hashed'))
+  }
+}))
+const salt = 10
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on succes', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value_to_hash')
+    expect(hash).toBe('hashed')
+  })
+
+  test('Should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+})
